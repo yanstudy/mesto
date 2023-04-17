5b60e209e7481acdde68bfe327159b4fd98f96a5
yanstudy <evr.studying@gmail.com>
2023-04-17 12:19:17 +0300

    fix: bugs

--- scripts/index.js
+++ scripts/index.js
@@ -3,33 +3,25 @@ let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close-icon");
 let name = document.querySelector(".profile__name");
 let job = document.querySelector(".profile__job");
+let formElement = document.querySelector(".popup__container");
+let nameInput = document.querySelector("#nameInput");
+let jobInput = document.querySelector("#jobInput");
 
 function showPopup() {
-  classesOfPopup.classList.add("popup_opened");
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
+  classesOfPopup.classList.add("popup_opened");
 }
-
 function closePopup() {
   classesOfPopup.classList.remove("popup_opened");
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
 }
-
-editButton.addEventListener("click", showPopup);
-closeButton.addEventListener("click", closePopup);
-
-let formElement = document.querySelector(".popup__container");
-
-let nameInput = document.querySelector(".popup__input-name");
-let jobInput = document.querySelector(".popup__input-job");
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
-
   closePopup();
 }
 
+editButton.addEventListener("click", showPopup);
+closeButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
